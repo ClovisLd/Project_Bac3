4fd07b705a5f6ddacc0d404cbada3b39c3b1ceaa
ClovisLd <clovis.leden@gmail.com>
2025-11-23 16:13:13 +0100

    restared form sratch

--- Project_Bac3/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Project_Bac3.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- Project_Bac3/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Project_Bac3.ViewModels;
-
-public class ViewModelBase : ObservableObject
-{
-}
